f4d6749ff7fc142908a9934bb531cebd1a0308fe
Thetnaing-mic <thet.naing.htun@mic-p.com>
2024-06-03 10:20:24 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from my_module import add
+
+# result1 = my_module.add(10, 5)
+# print(result1)
+
+# result2 = my_module.subtract(10, 5)
+# print(result2)
+
+# result3 = my_module.multiply(10, 5)
+# print(result3)
+print (add(1, 5))
