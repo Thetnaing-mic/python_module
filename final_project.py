133fa868cee7669f39e1ef6f89cac0b33c5d0d4c
Thetnaing-mic <thet.naing.htun@mic-p.com>
2024-06-10 13:13:22 +0900

    final_project_refector

--- final_project.py
+++ final_project.py
@@ -7,97 +7,92 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 
 load_dotenv()
 
-db_server = os.getenv('SERVER')
-db_database = os.getenv('DATABASE')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
+SERVER = os.getenv('SERVER')
+DATABASE = os.getenv('DATABASE')
+USER = os.getenv('DB_USER')
+PASSWORD = os.getenv('DB_PASSWORD')
 
-connection_string = (
-    f'DRIVER={{SQL Server}};'
-    f'SERVER={db_server};'
-    f'DATABASE={db_database};'
-    f'UID={db_user};'
-    f'PWD={db_password};'
-)
+connection_str = f'DRIVER={{SQL Server}}; SERVER={SERVER}; DATABASE={DATABASE}; UID={USER}; PWD={PASSWORD};'
+print(connection_str)
 
-conn = pyodbc.connect(connection_string)
-
-cursor = conn.cursor()
-print(conn)
+try:
+    conn = pyodbc.connect(connection_str)
+    cursor = conn.cursor()
+    print("Connection established successfully.")
+except pyodbc.Error as ex:
+    sqlstate = ex.args[1]
+    print(f"An error occurred: {sqlstate}")
 
 # 2
-create_table_query = '''
-CREATE TABLE my_movie (
-    ID INT PRIMARY KEY IDENTITY(1, 1),
-    Release_Date DATE,
-    Title VARCHAR(255),
-    Overview TEXT,
-    Popularity FLOAT,
-    Vote_Count INT,
-    Vote_Average FLOAT,
-    Original_Language VARCHAR(50),
-    Genre VARCHAR(255),
-    Poster_Url VARCHAR(255)
-)
-'''
-cursor.execute(create_table_query)
+# cursor.execute('''CREATE TABLE my_newmovie (
+#     ID INT PRIMARY KEY IDENTITY(1, 1),
+#     Release_Date DATE,
+#     Title VARCHAR(255),
+#     Overview VARCHAR(2000),
+#     Popularity FLOAT,
+#     Vote_Count INT,
+#     Vote_Average FLOAT,
+#     Original_Language VARCHAR(2),
+#     Genre VARCHAR(255),
+#     Poster_Url VARCHAR(255)
+# )''')
 
 
-# 3
-df = pd.read_csv("./mymoviedb.csv", lineterminator='\n')
-df.dropna(how = "all")
-print(df)
+# # 3
+# df = pd.read_csv("./mymoviedb.csv", lineterminator='\n')
+# df.dropna(how = "all")
+# print(df)
 
-# 4
-insert_query = '''
-INSERT INTO my_movie (Release_Date, Title, Overview, Popularity, Vote_Count, Vote_Average, Original_Language, Genre, Poster_Url)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-'''
-for index, row in df.iterrows():
-    cursor.execute(insert_query, 
-                    row['Release_Date'], 
-                    row['Title'], 
-                    row['Overview'], 
-                    row['Popularity'],
-                    row['Vote_Count'],
-                    row['Vote_Average'],
-                    row['Original_Language'], 
-                    row['Genre'], 
-                    row['Poster_Url'])
-conn.commit()
+# # 4
+# insert_query = '''
+# INSERT INTO my_movie (Release_Date, Title, Overview, Popularity, Vote_Count, Vote_Average, Original_Language, Genre, Poster_Url)
+# VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+# '''
+# for index, row in df.iterrows():
+#     cursor.execute(insert_query, 
+#                     row['Release_Date'], 
+#                     row['Title'], 
+#                     row['Overview'], 
+#                     row['Popularity'],
+#                     row['Vote_Count'],
+#                     row['Vote_Average'],
+#                     row['Original_Language'], 
+#                     row['Genre'], 
+#                     row['Poster_Url'])
+# conn.commit()
 
-# 5
-df = pd.read_sql("SELECT * FROM my_movie", conn)
-print(df)
+# # 5
+# df = pd.read_sql("SELECT * FROM my_movie", conn)
+# print(df)
 
-# 6
-language_count_query = "SELECT Original_Language, COUNT(*) AS Movie FROM my_movie GROUP BY Original_Language"
+# # 6
+# language_count_query = "SELECT Original_Language, COUNT(*) AS Movie FROM my_movie GROUP BY Original_Language"
 
-# 7
-genre_rating_query = "SELECT Genre, AVG(Vote_Average) AS Average_Rating FROM my_movie GROUP BY Genre"
+# # 7
+# genre_rating_query = "SELECT Genre, AVG(Vote_Average) AS Average_Rating FROM my_movie GROUP BY Genre"
 
-# 8
+# # 8
 
-language_count = pd.read_sql(language_count_query, conn)
-# print(language_count)
-genre_rating = pd.read_sql(genre_rating_query, conn)
-# print(genre_rating)
-wb = Workbook()
+# language_count = pd.read_sql(language_count_query, conn)
+# # print(language_count)
+# genre_rating = pd.read_sql(genre_rating_query, conn)
+# # print(genre_rating)
+# wb = Workbook()
 
-ws1 = wb.create_sheet(title = "Language Counts")
-for r in dataframe_to_rows(language_count, index=False, header=True):
-    ws1.append(r)
+# ws1 = wb.create_sheet(title = "Language Counts")
+# for r in dataframe_to_rows(language_count, index=False, header=True):
+#     ws1.append(r)
 
 
-ws2 = wb.create_sheet(title="Genre Ratings")
-for r in dataframe_to_rows(genre_rating, index=False, header=True):
-    ws2.append(r)
+# ws2 = wb.create_sheet(title="Genre Ratings")
+# for r in dataframe_to_rows(genre_rating, index=False, header=True):
+#     ws2.append(r)
 
-del wb['Sheet']
+# del wb['Sheet']
 
-wb.save('movies_analysis.xlsx')
+# wb.save('movies_analysis.xlsx')
 
-print("Analysis results saved to 'movies_analysis.xlsx'")
+# print("Analysis results saved to 'movies_analysis.xlsx'")
 
 # cursor.close()
-conn.close()
+# conn.close()
